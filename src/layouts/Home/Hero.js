f64697e47d557fb77d331e3e2ce7e123715cecd7
rupam-seal <rupam.x.seal@gmail.com>
2023-03-19 10:03:22 +0530

    🐛fix(*): items not visually hidden on mobile devices

--- src/layouts/Home/Hero.js
+++ src/layouts/Home/Hero.js
@@ -29,105 +29,88 @@ export const Hero = memo(({ sectionRef }) => {
       justify={'center'}
       ref={sectionRef}
     >
-      <Intro />
+      <Container
+        className={classes(styles.intro)}
+        direction="column"
+        zIndex={2}
+        animate="show"
+      >
+        <Text
+          size="m"
+          align="center"
+          weight="medium"
+          className={styles.name}
+          variants={textVariant(0.4)}
+        >
+          NILAX SEAL
+        </Text>
+        <Heading
+          level={0}
+          className={styles.title1}
+          align="center"
+          weight="medium"
+          variants={textVariant(0.2)}
+        >
+          ANDROID<span className={styles.line}>/</span>
+        </Heading>
+        <Heading
+          level={0}
+          className={styles.title2}
+          align="center"
+          weight="medium"
+          variants={textVariant(0.2)}
+        >
+          WEB DEVELOPER
+        </Heading>
+
+        <Text
+          size="m"
+          weight="medium"
+          align="center"
+          variants={textVariant(0)}
+          className={styles.description}
+          secondary
+        >
+          NilaX — Android / UI-UX / Web Developer freelance located in India,
+          specializing in the creation of modern and user freindly and unique
+          android and web designs.
+        </Text>
+      </Container>
       <Blob />
-      <Bottom />
+      <SectionBottom className={styles.sectionBottom}>
+        <Container
+          direction={'column'}
+          variants={scaleVariant}
+          className={styles.social}
+        >
+          <Social level={4} />
+        </Container>
+
+        <Container align="center" variants={textVariant(0.6)}>
+          <Container className={styles.arrow}></Container>
+          <Container direction={'column'} className={styles.scroll}>
+            <Text weight="regular" size="s">
+              Keep —
+            </Text>
+            <Text weight="regular" size="s">
+              Scrolling
+            </Text>
+          </Container>
+        </Container>
+
+        <Container direction={'column'} variants={scaleVariant}>
+          <Text weight="regular" size="s" align="start">
+            Available for work
+          </Text>
+          <Text className={styles.talk} weight="medium" size="s" align="start">
+            <A icon="leftCircle" link="/contact">
+              Lets talk
+            </A>
+          </Text>
+        </Container>
+      </SectionBottom>
     </Section>
   );
 });
-
-export const Intro = () => {
-  return (
-    <Container
-      className={classes(styles.intro)}
-      direction="column"
-      zIndex={2}
-      animate="show"
-    >
-      <Text
-        size="m"
-        align="center"
-        weight="medium"
-        className={styles.name}
-        variants={textVariant(0.4)}
-      >
-        NILAX SEAL
-      </Text>
-      <Heading
-        level={0}
-        className={styles.title1}
-        align="center"
-        weight="medium"
-        variants={textVariant(0.2)}
-      >
-        ANDROID<span className={styles.line}>/</span>
-      </Heading>
-      <Heading
-        level={0}
-        className={styles.title2}
-        align="center"
-        weight="medium"
-        variants={textVariant(0.2)}
-      >
-        WEB DEVELOPER
-      </Heading>
-
-      <Text
-        size="m"
-        weight="medium"
-        align="center"
-        variants={textVariant(0)}
-        className={styles.description}
-        secondary
-      >
-        NilaX — Android / UI-UX / Web Developer freelance located in India,
-        specializing in the creation of modern and user freindly and unique
-        android and web designs.
-      </Text>
-    </Container>
-  );
-};
-
-export const Bottom = () => {
-  return (
-    <SectionBottom className={styles.bottom}>
-      <Container
-        direction={'column'}
-        variants={scaleVariant}
-        className={styles.social}
-      >
-        <Social level={4} />
-      </Container>
-
-      <Container align="center" variants={textVariant(0.6)}>
-        <Container className={styles.arrow}></Container>
-        <Container direction={'column'} className={styles.keepScrolling}>
-          <Text weight="regular" size="s">
-            Keep —
-          </Text>
-          <Text weight="regular" size="s">
-            Scrolling
-          </Text>
-        </Container>
-      </Container>
-
-      <Container direction={'column'} variants={scaleVariant}>
-        <Text weight="regular" size="s" align="start">
-          Available for work
-        </Text>
-        <Text
-          className={styles.letsTalk}
-          weight="medium"
-          size="s"
-          align="start"
-        >
-          <A icon="leftCircle" link="/contact">
-            Lets talk
-          </A>
-        </Text>
-      </Container>
-    </SectionBottom>
-  );
-};
 
 Hero.displayName = 'Hero';
