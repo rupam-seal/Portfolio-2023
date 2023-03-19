daeacfa709456cafc61257f9130bfb147d095b16
rupam-seal <rupam.x.seal@gmail.com>
2023-03-19 11:10:16 +0530

    🐛fix(project): update styles

--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import styles from './slug.module.css';
+import styles from './Project.module.css';
 
 import { Section } from '../../components/Section';
 import { Heading } from '../../components/Heading';
