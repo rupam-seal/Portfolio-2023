eecbdb6c8d1ce05b3a755d43d560bd2e2ffa8748
rupam-seal <rupam.x.seal@gmail.com>
2023-03-23 11:04:37 +0530

    ✨feat(List): update meta item animation

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,24 +9,22 @@ import { Text } from '../Text';
 import styles from './List.module.css';
 
 export const List = ({ items, className }) => {
-  return (
-    <motion.ul
-      className={classes(styles.meta, className)}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true, amount: 0.25 }}
-    >
-      {!!items?.length &&
-        items?.map((item, index) => (
-          <motion.li
-            variants={textVariant(0.1 + index / 10)}
-            className={styles.metaItem}
-            key={item}
-          >
-            <Icon icon={item} />
-            <Text secondary>{item}</Text>
-          </motion.li>
-        ))}
-    </motion.ul>
-  );
+    return (
+        <motion.ul className={classes(styles.meta, className)}>
+            {!!items?.length &&
+                items?.map((item, index) => (
+                    <motion.li
+                        initial="hidden"
+                        whileInView="show"
+                        variants={textVariant(0.1 + index / 10)}
+                        viewport={{ once: true, amount: 0.25 }}
+                        className={styles.metaItem}
+                        key={item}
+                    >
+                        <Icon icon={item} />
+                        <Text secondary>{item}</Text>
+                    </motion.li>
+                ))}
+        </motion.ul>
+    );
 };
