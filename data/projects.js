cb22987d5292be6f2460a207736bc4914eaff330
rupam-seal <rupam.x.seal@gmail.com>
2023-03-23 23:57:46 +0530

    ✨feat(projects.js): update projects data

--- data/projects.js
+++ data/projects.js
@@ -1,176 +1,213 @@
 export const projects = [
-  {
-    id: 1,
-    title: 'Imo',
-    thumbnail: '/assets/images/imo/thumbnail.png',
-    image: '/assets/images/imo/thumbnail.png',
-    source: 'https://github.com/rupam-seal/imo',
-    live: '',
-    status:
-      'Future plans: Add some features like image details and share the image with others options and more.',
-    description:
-      'Imo is a image classification website where user can upload images and it will classify images into their respective category',
-    tags: ['Django', 'JavaScript', 'HTML', 'CSS'],
-    year: '2023',
-    category: 'web',
-    feature: [
-      {
+    {
         id: 1,
-        title: 'Upload Image',
-        image: '/assets/images/imo/demo1.png',
+        title: 'Imo',
+        thumbnail: '/assets/images/imo/thumbnail.png',
+        image: '/assets/images/imo/thumbnail.png',
+        source: 'https://github.com/rupam-seal/imo',
+        live: '',
+        status: 'Future plans: Add some features like image details and share the image with others options and more.',
         description:
-          'User can upload one or more images and it will classify them into their respective category',
-        status: '',
-      },
+            'Imo is a image classification website where user can upload images and it will classify images into their respective category',
+        tags: ['Django', 'JavaScript', 'HTML', 'CSS'],
+        year: '2023',
+        category: 'web',
+        feature: [
+            {
+                id: 1,
+                title: 'Upload Image',
+                image: '/assets/images/imo/demo1.png',
+                description:
+                    'User can upload one or more images and it will classify them into their respective category',
+                status: '',
+            },
 
-      {
+            {
+                id: 2,
+                title: 'Category',
+                image: '/assets/images/imo/demo2.png',
+                description:
+                    'All the images will be categoryized into their respective category and user can select each category individually to view the item on that category',
+                status: '',
+            },
+
+            {
+                id: 3,
+                title: 'Images',
+                image: '/assets/images/imo/demo3.png',
+                description: 'It shows the images on particular categories',
+                status: '',
+            },
+        ],
+        video: 'https://www.youtube.com/embed/w7nT0l8yMFk',
+    },
+
+    {
         id: 2,
-        title: 'Category',
-        image: '/assets/images/imo/demo2.png',
+        title: 'Cointra',
+        thumbnail: '/assets/images/cointra/thumbnail.png',
+        image: '/assets/images/cointra/thumbnail.png',
+        source: 'https://github.com/rupam-seal/CoinTra',
+        live: 'https://cointra.netlify.app/',
+        status: 'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
         description:
-          'All the images will be categoryized into their respective category and user can select each category individually to view the item on that category',
-        status: '',
-      },
+            'CoinTra is a website that displays the real-time value of the top 400 Cryptocurrencies along with their price, price change percentage, market cap value, volume, and other key metrics of cryptocurrencies. It also allows the user to search for a particular Coin.',
+        tags: ['React.js', 'JavaScript', 'HTML', 'CSS'],
+        year: '2023',
+        category: 'web',
+        feature: [
+            {
+                id: 1,
+                title: 'Main page',
+                image: '/assets/images/cointra/demo1.png',
+                description:
+                    'View all the crypto state directly on the main page and also user can search for a specific Coin',
+                status: '',
+            },
 
-      {
+            {
+                id: 2,
+                title: 'Single Coin page',
+                image: '/assets/images/cointra/demo2.png',
+                description:
+                    'Single coin page with price change percentage, market cap value, volume, and historical chart. For fetching historical data it uses Axios',
+                status: '',
+            },
+
+            {
+                id: 3,
+                title: 'Pagination',
+                image: '/assets/images/cointra/demo3.png',
+                description:
+                    'For navigating between pages. This component created using material ui.  ',
+                status: '',
+            },
+        ],
+        video: 'https://www.youtube.com/embed/xHz6DkeAF44',
+    },
+
+    {
         id: 3,
-        title: 'Images',
-        image: '/assets/images/imo/demo3.png',
-        description: 'It shows the images on particular categories',
-        status: '',
-      },
-    ],
-    video: 'https://www.youtube.com/embed/w7nT0l8yMFk',
-  },
-
-  {
-    id: 2,
-    title: 'Cointra',
-    thumbnail: '/assets/images/cointra/thumbnail.png',
-    image: '/assets/images/cointra/thumbnail.png',
-    source: 'https://github.com/rupam-seal/CoinTra',
-    live: 'https://cointra.netlify.app/',
-    status:
-      'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
-    description:
-      'CoinTra is a website that displays the real-time value of the top 400 Cryptocurrencies along with their price, price change percentage, market cap value, volume, and other key metrics of cryptocurrencies. It also allows the user to search for a particular Coin.',
-    tags: ['React.js', 'JavaScript', 'HTML', 'CSS'],
-    year: '2023',
-    category: 'web',
-    feature: [
-      {
-        id: 1,
-        title: 'Main page',
-        image: '/assets/images/cointra/demo1.png',
-        description:
-          'View all the crypto state directly on the main page and also user can search for a specific Coin',
-        status: '',
-      },
-
-      {
-        id: 2,
-        title: 'Single Coin page',
-        image: '/assets/images/cointra/demo2.png',
-        description:
-          'Single coin page with price change percentage, market cap value, volume, and historical chart. For fetching historical data it uses Axios',
-        status: '',
-      },
-
-      {
-        id: 3,
-        title: 'Pagination',
-        image: '/assets/images/cointra/demo3.png',
-        description:
-          'For navigating between pages. This component created using material ui.  ',
-        status: '',
-      },
-    ],
-    video: 'https://www.youtube.com/embed/xHz6DkeAF44',
-  },
-
-  {
-    id: 3,
-    title: 'ThreeD',
-    thumbnail: '/assets/images/threed/thumbnail.png',
-    image: '/assets/images/threed/thumbnail.png',
-    source: 'https://github.com/rupam-seal/drone-website',
-    live: 'https://threedrone.netlify.app/',
-    status:
-      'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
-    description: 'A drone website landing page using React and Three.js',
-    tags: ['React.js', 'Three.js', 'Blender', 'JavaScript', 'HTML', 'CSS'],
-    year: '2023',
-    category: 'web',
-    feature: [
-      {
-        id: 1,
-        title: 'Main page',
+        title: 'ThreeD',
+        thumbnail: '/assets/images/threed/thumbnail.png',
         image: '/assets/images/threed/thumbnail.png',
-        description:
-          'Currently this website have only one page. In the future i am thinking to add more features and pages.',
-        status: '',
-      },
-      {
-        id: 1,
-        title: 'Rotation of Propeller',
-        gif: '/assets/images/threed/fly.gif',
-        description:
-          'If any user hover on drone glb model propeller rotation start.',
-        status: '',
-      },
-      {
-        id: 1,
-        title: 'Change color',
-        gif: '/assets/images/threed/color.gif',
-        description:
-          'Click button to change drone color blue, red, yellow, green',
-        status: '',
-      },
-    ],
-    video: 'https://www.youtube.com/embed/NCiPE6CgJy8',
-  },
+        source: 'https://github.com/rupam-seal/drone-website',
+        live: 'https://threedrone.netlify.app/',
+        status: 'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
+        description: 'A drone website landing page using React and Three.js',
+        tags: ['React.js', 'Three.js', 'Blender', 'JavaScript', 'HTML', 'CSS'],
+        year: '2023',
+        category: 'web',
+        feature: [
+            {
+                id: 1,
+                title: 'Main page',
+                image: '/assets/images/threed/thumbnail.png',
+                description:
+                    'Currently this website have only one page. In the future i am thinking to add more features and pages.',
+                status: '',
+            },
+            {
+                id: 1,
+                title: 'Rotation of Propeller',
+                gif: '/assets/images/threed/fly.gif',
+                description:
+                    'If any user hover on drone glb model propeller rotation start.',
+                status: '',
+            },
+            {
+                id: 1,
+                title: 'Change color',
+                gif: '/assets/images/threed/color.gif',
+                description:
+                    'Click button to change drone color blue, red, yellow, green',
+                status: '',
+            },
+        ],
+        video: 'https://www.youtube.com/embed/NCiPE6CgJy8',
+    },
 
-  {
-    id: 4,
-    title: 'Card Printing',
-    thumbnail: '/assets/images/cardprinting/thumbnail.png',
-    image: '/assets/images/cardprinting/thumbnail.png',
-    source: 'https://github.com/rupam-seal/CardPrinting',
-    live: '',
-    status:
-      'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
-    description: 'A drone website landing page using React and Three.js',
-    tags: ['Python', 'Three.js'],
-    year: '2023',
-    category: 'desktop',
-    feature: [
-      {
-        id: 1,
-        title: 'Login Page',
-        image: '/assets/images/cardprinting/login.png',
-        description:
-          'User can login from this page and after login user is redirected to home page',
-        status: '',
-      },
-      {
-        id: 2,
-        title: 'Home Page',
-        image: '/assets/images/cardprinting/main.png',
-        description:
-          'User can enter value and after click on print software will print hardcopy of the card with printer.',
-        status: '',
-      },
-      {
-        id: 3,
-        title: 'Print page',
-        image: '/assets/images/cardprinting/print.png',
-        description:
-          'After clicking on print button it will generate a new card from above information.',
-        status: '',
-      },
-    ],
-    video: 'https://www.youtube.com/embed/CnM-VT5AU4Y',
-  },
+    {
+        id: 4,
+        title: 'Card Printing',
+        thumbnail: '/assets/images/cardprinting/thumbnail.png',
+        image: '/assets/images/cardprinting/thumbnail.png',
+        source: 'https://github.com/rupam-seal/CardPrinting',
+        live: '',
+        status: 'Future plans: Add top 5 coins carousel and add more feature like chat options and more.',
+        description: 'A drone website landing page using React and Three.js',
+        tags: ['Python', 'Three.js'],
+        year: '2023',
+        category: 'desktop',
+        feature: [
+            {
+                id: 1,
+                title: 'Login Page',
+                image: '/assets/images/cardprinting/login.png',
+                description:
+                    'User can login from this page and after login user is redirected to home page',
+                status: '',
+            },
+            {
+                id: 2,
+                title: 'Home Page',
+                image: '/assets/images/cardprinting/main.png',
+                description:
+                    'User can enter value and after click on print software will print hardcopy of the card with printer.',
+                status: '',
+            },
+            {
+                id: 3,
+                title: 'Print page',
+                image: '/assets/images/cardprinting/print.png',
+                description:
+                    'After clicking on print button it will generate a new card from above information.',
+                status: '',
+            },
+        ],
+        video: 'https://www.youtube.com/embed/CnM-VT5AU4Y',
+    },
+
+    {
+        id: 5,
+        title: 'Portfolio 2022',
+        thumbnail: '/assets/images/portfolio2022/thumbnail.png',
+        image: '/assets/images/portfolio2022/thumbnail.png',
+        source: 'https://github.com/rupam-seal/portfolio-2022.git',
+        live: 'https://nilax-2022.netlify.app/',
+        status: 'Need to update code, refractor complete project and improve performance.',
+        description: 'Portfolio website made with reactjs',
+        tags: ['Python', 'Three.js'],
+        year: '2023',
+        category: 'web',
+        feature: [
+            {
+                id: 1,
+                title: 'Login Page',
+                image: '/assets/images/cardprinting/login.png',
+                description:
+                    'User can login from this page and after login user is redirected to home page',
+                status: '',
+            },
+            {
+                id: 2,
+                title: 'Home Page',
+                image: '/assets/images/cardprinting/main.png',
+                description:
+                    'User can enter value and after click on print software will print hardcopy of the card with printer.',
+                status: '',
+            },
+            {
+                id: 3,
+                title: 'Print page',
+                image: '/assets/images/cardprinting/print.png',
+                description:
+                    'After clicking on print button it will generate a new card from above information.',
+                status: '',
+            },
+        ],
+        video: 'https://www.youtube.com/embed/CnM-VT5AU4Y',
+    },
 ];
 
 // info: [
