97995204062c140151c65d23191361229d7c250d
rupam-seal <rupam.x.seal@gmail.com>
2023-03-25 09:59:29 +0530

    ✨feat(About):update description

--- src/layouts/Home/About.js
+++ src/layouts/Home/About.js
@@ -15,79 +15,96 @@ import { Icon } from '@/components/Icon';
 import { memo } from 'react';
 
 export const About = memo(({ visible, sectionRef }) => {
-  return (
-    <Section
-      direction={'column'}
-      className={styles.section}
-      align={'center'}
-      justify={'center'}
-      ref={sectionRef}
-    >
-      {visible && (
-        <>
-          <Container>
-            <Heading level={1} weight="Bold" variants={textVariant(0.1)}>
-              ABOUT ME
-            </Heading>
-          </Container>
-          <Section className={styles.content}>
-            <Container className={styles.image} align="center" justify="center">
-              <Image
-                src={'/assets/images/profile.jpg'}
-                href="/"
-                variants={imageVariant('0.4')}
-                rounded={true}
-              />
-            </Container>
-            <Container
-              className={styles.details}
-              align="left"
-              direction="column"
-            >
-              <Container align="center" variants={textVariant(0.1)}>
-                <Heading level={3} size="m" weight="medium">
-                  Hi there
-                </Heading>
-                <Container className={styles.waveContainer}>
-                  <span className={styles.wave}>👋</span>
-                </Container>
-              </Container>
-              <Text
-                size="l"
-                weight="regular"
-                secondary
-                variants={textVariant(0.3)}
-              >
-                I am a Full-Stack <A link={'/'}>Android</A> and{' '}
-                <A link={'/'}>Web</A> developer. I started learning to code when
-                I was a teenager. Well-versed in technology and writing code to
-                create systems that are reliable and user-friendly.
-              </Text>
-              <Text
-                size="l"
-                weight="regular"
-                secondary
-                variants={textVariant(0.4)}
-              >
-                I’ve worked on many freelance <A link={'/'}>projects</A>. I’m
-                familiar with various programming languages, including Java,
-                C++, Python, and JavaScript but I’m always adding new skills to
-                my repertoire.
-              </Text>
-              <Button
-                variants={textVariant(0.35)}
-                href={'/contact'}
-                size="m"
-                icon={'send'}
-              >
-                Send message
-              </Button>
-            </Container>
-          </Section>
-        </>
-      )}
-    </Section>
-  );
+    return (
+        <Section
+            direction={'column'}
+            className={styles.section}
+            align={'center'}
+            justify={'center'}
+            ref={sectionRef}
+        >
+            {visible && (
+                <>
+                    <Container>
+                        <Heading
+                            level={1}
+                            weight="Bold"
+                            variants={textVariant(0.1)}
+                        >
+                            ABOUT ME
+                        </Heading>
+                    </Container>
+                    <Section className={styles.content}>
+                        <Container
+                            className={styles.image}
+                            align="center"
+                            justify="center"
+                        >
+                            <Image
+                                src={'/assets/images/profile.jpg'}
+                                href="/"
+                                variants={imageVariant('0.4')}
+                                rounded={true}
+                            />
+                        </Container>
+                        <Container
+                            className={styles.details}
+                            align="left"
+                            direction="column"
+                        >
+                            <Container
+                                align="center"
+                                variants={textVariant(0.1)}
+                            >
+                                <Heading level={3} size="m" weight="medium">
+                                    Hi there
+                                </Heading>
+                                <Container className={styles.waveContainer}>
+                                    <span className={styles.wave}>👋</span>
+                                </Container>
+                            </Container>
+                            <Text
+                                size="m"
+                                weight="regular"
+                                secondary
+                                variants={textVariant(0.3)}
+                            >
+                                I'm a self-taught android and web developer with
+                                a passion for coding and creating engaging web
+                                experiences. I started learning to code when I
+                                was a teenager. I've taught myself various
+                                programming languages, frameworks, and
+                                technologies, and I have hands-on experience in
+                                both <A link={'/projects'}>front-end</A> and{' '}
+                                <A link={'/projects'}>back-end</A> development
+                                and deployment.
+                            </Text>
+                            <Text
+                                size="m"
+                                weight="regular"
+                                secondary
+                                variants={textVariant(0.3)}
+                            >
+                                I'm a fast learner and a problem-solver, always
+                                looking for innovative solutions to complex
+                                problems. I love what I do and am excited about
+                                the endless possibilities for innovation and
+                                creativity in software development.
+                            </Text>
+                            <Button
+                                variants={textVariant(0.35)}
+                                href={'/contact'}
+                                size="m"
+                                icon={'send'}
+                            >
+                                Send message
+                            </Button>
+                        </Container>
+                    </Section>
+                </>
+            )}
+        </Section>
+    );
 });
 
 About.displayName = 'About';
