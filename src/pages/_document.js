3b3773bc4288f0aed06f8d2a324256bb61ef6e4c
rupam-seal <rupam.x.seal@gmail.com>
2023-03-29 14:21:28 +0530

    ✨feat(Fonts): preload fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import GothamBook from '../assets/fonts/gotham-book.woff2';
+import GothamMedium from '../assets/fonts/gotham-medium.woff2';
+import Impact from '../assets/fonts/impact.woff';
 
 export default function Document() {
   return (
@@ -12,6 +15,9 @@ export default function Document() {
           rel="prefetch"
           href="https://prod.spline.design/MYdCFyAC5T06Rt9l/scene.splinecode"
         />
+
+        <link rel="preload" href={GothamMedium} as="font" crossOrigin="true" />
+        <link rel="preload" href={GothamBook} as="font" crossOrigin="true" />
       </Head>
       <body>
         <Main />
