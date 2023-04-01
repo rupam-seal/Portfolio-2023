0402080f1b29bbebb527f086a10537d13cd6c374
rupam-seal <rupam.x.seal@gmail.com>
2023-04-01 19:38:09 +0530

    ✨feat(socialData.js): update social data

--- data/socialData.js
+++ data/socialData.js
@@ -6,12 +6,12 @@ export const social = [
   },
   {
     label: 'Linkedin',
-    link: 'https://github.com/rupam-seal',
+    link: 'https://www.linkedin.com/in/rupamseal/',
     target: '_blank',
   },
   {
     label: 'Instagram',
-    link: 'https://github.com/rupam-seal',
+    link: 'https://www.instagram.com/ophtrus/',
     target: '_blank',
   },
 ];
